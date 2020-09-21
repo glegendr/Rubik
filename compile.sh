1197189e205c9c0338d40327cabefc74dc41c787
Gaspard Legendre <glegendr@student.42.fr>
2020-09-21 17:41:59 +0200

    Feat: make the world better

--- compile.sh
+++ compile.sh
@@ -1,1 +1,7 @@
 stack build && stack ghc src/Main.hs -- -O2 -threaded && rm src/*.o src/*.hi && mv src/Main Rubik
+
+# if [ $0 -eq "run" || $0 -eq "execute" ]
+if [ $1 = "run" ] || [ $1 = "execute" ]
+then
+    ./Rubik
+fi
